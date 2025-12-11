c645e71b556573babc131540af0fc7956415dd7e
MADANA-GS <madangsnaik@gmail.com>
2025-12-11 11:47:42 +0530

    Initialize server with Express, add essential middleware, and set up basic routing

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
