6579f3d8a685fa4a4b12c7186aebefe270a01b49
MADANA-GS <madangsnaik@gmail.com>
2025-12-20 23:06:58 +0530

    feat: Update Google OAuth configuration to use environment variables

--- server/config/google.js
+++ server/config/google.js
@@ -1,9 +1,9 @@
 import { google } from "googleapis";
-// import { OAuth2Client } from "google-auth-library";
-// import dotenv from "dotenv";
+import { OAuth2Client } from "google-auth-library";
+import dotenv from "dotenv";
 
-// dotenv.config();
+dotenv.config();
 export const client = new google.auth.OAuth2(
-  "168486816704-4j71fl57h4vg5f5vf815pk8f62njddst.apps.googleusercontent.com",
-  "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",
+  process.env.GOOGLE_CLIENT_ID,
+  process.env.GOOGLE_CLIENT_SECRET
 );
