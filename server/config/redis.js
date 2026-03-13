cd0027ce3ee9eaebf12083cd2488d41595c5ed26
MADANA-GS <madangsnaik@gmail.com>
2026-03-13 15:36:08 +0530

    fix: simplify dotenv configuration by removing path specification

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,5 +1,5 @@
 import dotenv from "dotenv";
-dotenv.config({ path: "../../.env" });
+dotenv.config();
 import IORedis from "ioredis";
 
 export const pubClient = new IORedis(process.env.REDIS_URL, {
